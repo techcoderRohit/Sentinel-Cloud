f9e2bf206fa11255e5822050c98d633b27d25dd7
techCoderRohit <rohitmodi0705@gmail.com>
2026-02-24 09:37:25 +0530

    changes

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -2,33 +2,33 @@ import Link from 'next/link';
 import { Mail, Lock } from 'lucide-react';
 export default function Login() {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-slate-900 px-4 py-8">
-      <div className="w-full max-w-md p-8 bg-white rounded-xl shadow-lg border border-slate-200">
+    <div className="flex justify-center items-center min-h-screen bg-slate-950 px-4 py-8">
+      <div className="w-full max-w-md p-8 bg-slate-900 rounded-xl shadow-2xl border border-slate-800">
         <div className="text-center mb-8">
-          <h2 className="text-3xl font-bold text-slate-900">Welcome Back</h2>
-          <p className="text-slate-500 mt-2">Login to your Sentinel Cloud account</p>
+          <h2 className="text-3xl font-bold text-cyan-400">Welcome Back</h2>
+          <p className="text-slate-500 mt-2">Login to Sentinal Cloud account</p>
         </div>
-        
+
         <form className="space-y-5">
           <div>
-            <label className=" text-slate-700 mb-2">Email</label>
-            <div className='flex items-center border border-slate-300 rounded-lg mt-2 px-2 focus-within:ring-1 focus-within:ring-teal-400 transition duration-200 '>
-                <Mail className='text-slate-400 size={18}'/>
+            <label className=" text-slate-300 mb-2">Email</label>
+            <div className='flex items-center bg-slate-800 border border-slate-700 rounded-lg mt-1 mb-2 px-2 focus-within:ring-2 focus-within:ring-cyan-500 transition duration-200 '>
+                <Mail className='text-cyan-500 size={18}'/>
             <input 
               type="email" 
               placeholder="Enter your Email"
-              className="w-full px-4 py-2 text-slate-900 outline-none " 
+              className="w-full px-4 py-2 text-white placeholder-slate-400 outline-none" 
             />
             </div>
             </div>
           <div>
-            <label className="block text-slate-700 mb-2">Password</label>
-            <div className='flex items-center border border-slate-300 rounded-lg mt-2 px-2 focus-within:ring-1 focus-within:ring-teal-400 transition duration-200'>
-                <Lock className='text-slate-400 size={18}'/>
+            <label className=" text-slate-300 mb-2">Password</label>
+            <div className='flex items-center bg-slate-800 border border-slate-700 rounded-lg mt-1 mb-2 px-2 focus-within:ring-2 focus-within:ring-cyan-500 transition duration-200'>
+                <Lock className='text-cyan-500 size={18}'/>
                 <input 
               type="password" 
               placeholder="Enter your password"
-              className="w-full px-4 py-2 text-slate-900 outline-none " 
+              className="w-full px-4 py-2 text-white placeholder-slate-400 outline-none " 
             />
             </div>
             
@@ -38,25 +38,25 @@ export default function Login() {
             <label className="flex items-center">
               <input 
                 type="checkbox" 
-                className="mr-2 rounded text-blue-600 focus:ring-blue-700" 
+                className="mr-2 rounded text-indigo-600 focus:ring-indigo-700" 
               />
-              <span className="text-slate-600">Remember me</span>
+              <span className="text-slate-500">Remember me</span>
             </label>
-            <Link href="#" className="text-indigo-500 hover:text-indigo-800 hover:underline text-sm">
+            <Link href="#" className="text-cyan-400 hover:text-cyan-500 hover:underline text-sm">
               Forgot password?
             </Link>
           </div>
 
           <button 
             type="submit"
-            className="w-full px-4 py-2 bg-teal-600 text-white font-bold rounded-md hover:bg-teal-800 transition shadow-sm">
+            className="w-full px-4 py-2 bg-gradient-to-r from-cyan-500 to-blue-500 text-white font-bold rounded-lg hover:from-cyan-400 hover:to-blue-400 transition duration-300 shadow-2xl">
             Login
           </button>
         </form>
 
-        <div className="mt-6 text-center text-sm text-slate-600">
+        <div className="mt-6 text-center text-sm text-slate-500">
           Don't have an account? 
-          <Link href="/Signup" className="text-indigo-600 font-medium hover:text-indigo-800 hover:underline">  Signup</Link>
+          <Link href="/Signup" className="text-cyan-400 ml-2 font-semibold hover:text-cyan-500 hover:underline">Signup</Link>
         </div>
       </div>
     </div>
--- frontend/src/components/SignupForm.jsx
+++ frontend/src/components/SignupForm.jsx
@@ -2,55 +2,55 @@ import Link from 'next/link';
 import { User, Mail , Lock } from 'lucide-react';
 export default function Signup() {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-slate-900 px-4 py-8">
-      <div className="w-full max-w-md p-8 bg-white rounded-xl shadow-lg border border-slate-200">
+    <div className="flex justify-center items-center min-h-screen bg-slate-950 px-4 py-8">
+      <div className="w-full max-w-md p-8 bg-slate-900 rounded-xl shadow-lg border border-slate-800">
         <div className="text-center mb-8">
-          <h2 className="text-3xl font-bold text-slate-900">Create an Account</h2>
+          <h2 className="text-3xl font-bold text-cyan-400">Create an Account</h2>
           <p className="text-slate-500 mt-2">Start monitoring and managing your IoT devices</p>
         </div>
         
         <form className="space-y-5">
           <div>
-            <label className=" text-slate-700 mb-2">Full Name</label>
-            <div className='flex items-center border border-slate-300 rounded-lg mt-2 px-2 focus-within:ring-1 focus-within:ring-teal-400 transition duration-200 '>
-                <User className='text-slate-400 size={18}'/>
+            <label className=" text-slate-300 mb-2">Full Name</label>
+            <div className='flex items-center bg-slate-800 border border-slate-700 rounded-lg mt-1 mb-2 px-2 focus-within:ring-2 focus-within:ring-cyan-500 transition duration-200 '>
+                <User className='text-cyan-500 size={18}'/>
             <input 
               type="text" 
               placeholder="Enter your name"
-              className="w-full px-4 py-2 text-slate-900 outline-none " 
+              className="w-full px-4 py-2 text-white placeholder-slate-400 outline-none " 
             />
             </div>
             </div>
             <div>
-            <label className=" text-slate-700 mb-2">Email</label>
-            <div className='flex items-center border border-slate-300 rounded-lg mt-2 px-2 focus-within:ring-1 focus-within:ring-teal-400 transition duration-200 '>
-                <Mail className='text-slate-400 size={18}'/>
+            <label className=" text-slate-300 mb-2">Email</label>
+            <div className='flex items-center bg-slate-800 border border-slate-700 rounded-lg mt-1 mb-2 px-2 focus-within:ring-2 focus-within:ring-cyan-500 transition duration-200 '>
+                <Mail className='text-cyan-500 size={18}'/>
             <input 
               type="email" 
               placeholder="Enter your email"
-              className="w-full px-4 py-2 text-slate-900 outline-none " 
+              className="w-full px-4 py-2 text-white placeholder-slate-400 outline-none " 
             />
             </div>
             </div>
             <div>
-            <label className=" text-slate-700 mb-2">Password</label>
-            <div className='flex items-center border border-slate-300 rounded-lg mt-2 px-2 focus-within:ring-1 focus-within:ring-teal-400 transition duration-200 '>
-                <Lock className='text-slate-400 size={18}'/>
+            <label className=" text-slate-300 mb-2">Password</label>
+            <div className='flex items-center bg-slate-800 border border-slate-700 rounded-lg mt-1 mb-2 px-2 focus-within:ring-2 focus-within:ring-cyan-500 transition duration-200 '>
+                <Lock className='text-cyan-500 size={18}'/>
             <input 
               type="password" 
               placeholder="Enter your password"
-              className="w-full px-4 py-2 text-slate-900 outline-none " 
+              className="w-full px-4 py-2 text-white placeholder-slate-400 outline-none " 
             />
             </div>
             </div>
             <div>
-            <label className=" text-slate-700 mb-2">Confirm Password</label>
-            <div className='flex items-center border border-slate-300 rounded-lg mt-2 px-2 focus-within:ring-1 focus-within:ring-teal-400 transition duration-200 '>
-                <Lock className='text-slate-400 size={18}'/>
+            <label className=" text-slate-300 mb-2">Confirm Password</label>
+            <div className='flex items-center bg-slate-800 border border-slate-700 rounded-lg mt-2 px-2 focus-within:ring-1 focus-within:ring-cyan-500 transition duration-200 '>
+                <Lock className='text-cyan-500 size={18}'/>
             <input 
               type="password" 
               placeholder="Enter your confirm password"
-              className="w-full px-4 py-2 text-slate-900 outline-none " 
+              className="w-full px-4 py-2 text-white placeholder-slate-400 outline-none " 
             />
             </div>
             </div>
@@ -58,23 +58,23 @@ export default function Signup() {
             <input 
               type="checkbox" 
               id="terms" 
-              className="mt-1 mr-2 rounded text-blue-600 focus:ring-blue-500" 
+              className="mt-1 mr-2 rounded text-indigo-600 focus:ring-indigo-500" 
             />
-            <label htmlFor="terms" className="text-sm text-slate-600">
-              I agree to the <Link href="/" className="text-indigo-500 hover:text-indigo-800 hover:underline">Terms of Service</Link> and <Link href="/" className="text-indigo-600 hover:text-indigo-500 hover:underline">Privacy Policy</Link>.
+            <label htmlFor="terms" className="text-sm text-slate-500">
+              I agree to the <Link href="/" className="text-cyan-400 hover:text-cyan-500 hover:underline">Terms of Service</Link> and <Link href="/" className="text-cyan-400 hover:text-cyan-500 hover:underline">Privacy Policy</Link>.
             </label>
           </div>
 
           <button 
             type="submit"
-            className="w-full px-4 py-2 bg-teal-600 text-white font-semibold rounded-md hover:bg-teal-800 transition shadow-sm"
+            className="w-full px-4 py-2 bg-gradient-to-r from-cyan-500 to-blue-500 text-white font-semibold rounded-lg hover:from-cyan-400 hover:to-blue-400 transition duration-300 shadow-2xl"
           >
             Create Account
           </button>
         </form>
 
-        <div className="mt-8 text-center text-sm text-slate-600">
-          Already have an account? <Link href="/login" className="text-indigo-600 font-medium hover:underline hover:text-indigo-800">  Login</Link>
+        <div className="mt-8 text-center text-sm text-slate-500">
+          Already have an account? <Link href="/login" className="text-cyan-400 font-semibold hover:underline hover:text-cyan-500">  Login</Link>
         </div>
       </div>
     </div>
