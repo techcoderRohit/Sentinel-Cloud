34d956865fe4d13516d949242319e2214db1db97
techCoderRohit <rohitmodi0705@gmail.com>
2026-03-21 16:58:36 +0530

    changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,14 @@
 import http from "http";
 import app from "./app.js";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+connectDB();
 
 const server = http.createServer(app);
-const PORT = "5000";
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, ()=>{
     console.log(`Server running on ${PORT}`);
