7090cb95c40bf747c73103869844219b7c1016de
devsamir13 <samir5827245@gmail.com>
2026-04-04 11:55:44 +0530

    SignUp Changes

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,5 +1,8 @@
 const mongoose = require( 'mongoose');
 
+const dns = require("node:dns/promises");  
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI);
